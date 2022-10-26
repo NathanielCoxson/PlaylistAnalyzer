17a34d15c4305d239b23950bab3a5397499efb52
Nathan Coxson <nathancoxson2739@gmail.com>
2022-10-26 06:41:03 -0400

    Added Spotify module with a getAccessToken function

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import {React, useState} from 'react';
 import {SearchBar} from '../SearchBar/SearchBar';
+import Spotify from '../../utils/Spotify';
 
 function App() {
     const [id, setId] = useState();
+    const accessToken = Spotify.getAccessToken();
     return (
         <div className="App">
             <h1>Spotify Playlist Analyzer</h1>
@@ -11,6 +13,7 @@ function App() {
                 onSubmit={setId}
             />
             <p>{id}</p>
+            <p>{accessToken}</p>
         </div>
     );
 }
---
+++ src/utils/Spotify.js
@@ -0,0 +1,28 @@
+let accessToken = '';
+const clientId = '3a32785f7f184abfaa9f921c96f3c2dc';
+const redirectUri = 'http://localhost:3000/';
+
+const Spotify = {
+    getAccessToken() {
+        if(accessToken) {
+            return accessToken;
+        }
+
+        const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+        const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+        if(accessTokenMatch && expiresInMatch) {
+            accessToken = accessTokenMatch[1];
+            const expiresIn = Number(expiresInMatch[1]);
+            window.setTimeout(() => accessToken = '', expiresIn * 1000);
+            window.history.pushState('Access Token', null, '/');
+            return accessToken;
+        }
+        else {
+            const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+            window.location = accessUrl;
+        }
+    }
+};
+
+export default Spotify;
