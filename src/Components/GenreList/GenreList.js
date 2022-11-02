f7ce65d482c6827bc5a915256bb4e30c4bedcc0c
Nathan Coxson <nathancoxson2739@gmail.com>
2022-11-02 02:04:51 -0400

    Updated Spotify URIto work with main site

--- src/Components/GenreList/GenreList.js
+++ src/Components/GenreList/GenreList.js
@@ -3,13 +3,35 @@ import React from 'react';
 
 export function GenreList(props) {
     const {genres, genreCounts} = props;
+
+    // let totalCount = 0;
+    // for(const key in genreCounts) {
+    //     totalCount += genreCounts[key];
+    // }
+
     return (
         <div className='GenreList'>
             <ul>
                 {genres.map((entry, i) => {
-                    return <li key={i} style={{textAlign: 'left'}}>{genreCounts[entry]} {entry}</li>
+                    return <li key={i}>{genreCounts[entry]} {entry}</li>
                 })}
             </ul>
+            {/* {genres.map((entry, i) => {
+                const percentage = (genreCounts[entry]/totalCount) * 100;
+                console.log(percentage);
+                return (
+                    <div>
+                        <div
+                            className='bar'
+                            style={{
+                                width: `${percentage}%`
+                            }}
+                        >
+                        </div>
+                        <p>{entry}</p>
+                    </div>   
+                )
+            })} */}
         </div>
     );
 }
